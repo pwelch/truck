dff747c5c58f87d0a6d837f6a886d1365c2a2c86
pwelch <pwelch@chef.io>
2018-02-22 14:46:13 -0500

    Working file chunk

--- Makefile
+++ Makefile
@@ -1,12 +1,17 @@
 default: proto
 
+build: server client
+
 server:
 	go build server.go
 
 client:
-	go build -o foo client/client.go
+	go build -o cli client/client.go
+
+clean:
+	rm server cli output.txt
 
 proto:
 	protoc -I ./protobuf --go_out=plugins=grpc:./protobuf ./protobuf/*.proto
 
-.PHONY: proto server client
+.PHONY: proto server client build clean
--- server.go
+++ server.go
@@ -3,11 +3,16 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"net"
+	"os"
 
 	pb "github.com/pwelch/truck/protobuf"
 	grpc "google.golang.org/grpc"
 )
+
+// bufferSize for file transfer, default 0.25MB
+const bufferSize = 262144
 
 // FileServer type
 type FileServer struct{}
@@ -16,11 +21,32 @@ type FileServer struct{}
 func (s *FileServer) FileTransfer(req *pb.Request, stream pb.File_FileTransferServer) error {
 	fmt.Println("Entering FileTransfer")
 
-	start := []byte("start")
-	end := []byte("end")
+	// Open our file to transfer
+	file, err := os.Open("./fixtures/time_machine.txt")
+	if err != nil {
+		fmt.Println("err reading file")
+	}
+	defer file.Close()
 
-	stream.Send(&pb.Response{Content: start})
-	stream.Send(&pb.Response{Content: end})
+	// Create our buffer to chunk the file
+	buffer := make([]byte, bufferSize)
+
+	// Read the file until we hit EOF
+	for {
+		// Read read up to len(buffer) bytes from the file
+		bytesRead, err := file.Read(buffer)
+		if err != nil {
+			if err != io.EOF {
+				fmt.Println(err)
+			}
+
+			break
+		}
+
+		// Send this chunk of bytes.
+		// buffer[:bytesRead] ensures we send the remaining bytes if buffer is not full
+		stream.Send(&pb.Response{Content: buffer[:bytesRead]})
+	}
 
 	fmt.Println("Finished FileTransfer")
 	return nil
